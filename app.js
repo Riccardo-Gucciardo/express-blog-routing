7e0f65fe6bcf91feef380666cdc25abd65b6fc74
Riccardo-Gucciardo <riccardogucciardo003@gmail.com>
2025-02-12 10:18:57 +0100

    corretto a lezione

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express()
 const port =3000
-const postsrouter = require(/routers/posts)
+const postsrouter = require('./routers/posts')
 
-app.use("/posts",postsrouter)
+app.use('/',(req,res)=>{
+    res.send('hello world')
+})
+
+app.use("/api/posts",postsrouter)
 
 
 app.listen(port, ()=>{
--- routers/posts.js
+++ routers/posts.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const app = express()
 
 // index
-router.get('/', function (req, res) {
+    router.get('/', function (req, res) {
     res.send('Lista delle pizze');
     });
     // show
     router.get('/:id', function (req, res) {
-    res.send('Dettagli della pizza ' + req.params.id);
+    res.send(`Dettagli della pizza ${req.params.id}`);
     });
     // store
     router.post('/', function (req, res) {
@@ -16,15 +16,15 @@ router.get('/', function (req, res) {
     });
     // update
     router.put('/:id', function (req, res) {
-    res.send('Modifica integrale della pizza ' + req.params.id);
+    res.send(`Modifica integrale della pizza ${req.params.id} `);
     });
     // modify
     router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale della pizza ' + req.params.id);
+    res.send(`Modifica parziale della pizza ${req.params.id} `);
     });
     // destroy
     router.delete('/:id', function (req, res) {
-    res.send('Eliminazione della pizza ' + req.params.id);
+    res.send(`Eliminazione della pizza  ${req.params.id}`);
     });
 
 
